ec39036630ff603a64f8e0c0427b8509633072ab
Stephen Andrews <stephen@andrewstribe.co.nz>
2021-10-16 10:31:21 +1300

    update

--- src/views/Sale.js
+++ src/views/Sale.js
@@ -284,6 +284,7 @@ export default function Sale(props) {
   const [view, setView] = React.useState(false);
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [salesOnline, setSalesOnline] = React.useState(false);
+  const [loaded, setLoaded] = React.useState(false);
   const [sold, setSold] = React.useState(0);
   const [unsold, setUnsold] = React.useState(9937);
   var publicPrice = 150000000n;
@@ -292,12 +293,11 @@ export default function Sale(props) {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
+  
   const _updates = async () => {
-    console.log('updating');
     if (!address) return;
     var stats = await api.token("ahl3d-xqaaa-aaaaj-qacca-cai").call.salesStats(address);
-    console.log(stats);
+    if (!loaded) setLoaded(true);
     setSalesOnline(stats[0]);
     setUnsold(Number(stats[1]));
     setSold(Number(stats[2]));
@@ -515,6 +515,10 @@ export default function Sale(props) {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [identity]);
+  React.useEffect(() => {
+    _updates();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [address]);
   const getRemaining = ti => {
     ti += 1000;
     if (ti < 2000) return 2000-ti;
@@ -588,71 +592,76 @@ export default function Sale(props) {
           <br />
           <br />
           <p><strong>We will have all of the 10,000 NFTs available for sale at launch, there will be 5 pirce tiers (Thirty NFTs will be reserved for the creators). We will try to distrube them fairly as possible by whitelisting.</strong></p>
-          {salesOnline ? 
-          <>
-            <p><strong>Please note:</strong> All transactions are secured via Entrepot's escrow platform. There are no refunds or returns, once a transaction is made it can not be reversed. Entrepot provides a transaction service only. By clicking the button below you show acceptance of our Terms of Service</p>
-            <Grid container spacing={2} style={{}}>
-              <Grid className={classes.stat} item sm={3}>
-            <Button
-              variant={"contained"}
-              color={"primary"}
-              onClick={() => buyFromSale(1)}
-              style={{ fontWeight: "bold", margin: "0 auto" }}
-            >
-              Buy 1 ICTuT<br />for {_showListingPrice(150000000n)} ICP
-            </Button>
-            </Grid>
-              <Grid className={classes.stat} item sm={3}>
-            <Button
-              variant={"contained"}
-              color={"primary"}
-              onClick={() => buyFromSale(5)}
-              style={{ fontWeight: "bold", margin: "0 auto" }}
-            >
-              Buy 5 ICTuT<br />for {_showListingPrice(150000000n*5n)} ICP
-            </Button>
-            </Grid>
-              <Grid className={classes.stat} item sm={3}>
-            <Button
-              variant={"contained"}
-              color={"primary"}
-              onClick={() => buyFromSale(10)}
-              style={{ fontWeight: "bold", margin: "0 auto" }}
-            >
-              Buy 10 ICTuT<br />for {_showListingPrice(150000000n*10n)} ICP
-            </Button>
-            </Grid>
-              <Grid className={classes.stat} item sm={3}>
-            <Button
-              variant={"contained"}
-              color={"primary"}
-              onClick={() => buyFromSale(20)}
-              style={{ fontWeight: "bold", margin: "0 auto" }}
-            >
-              Buy 20 ICTuT<br />for {_showListingPrice(150000000n*20n)} ICP
-            </Button>
-            </Grid>
-            </Grid>
-            
-            </>
-          :
+          {!address ?
             <>
-            {whitelistPrice > 0 && whitelistPrice < 150000000n ?
-          <>
-            <p><strong>Please note:</strong> All transactions are secured via Entrepot's escrow platform. There are no refunds or returns, once a transaction is made it can not be reversed. Entrepot provides a transaction service only. By clicking the button below you show acceptance of our Terms of Service</p>
-            <Button
-              variant={"contained"}
-              color={"primary"}
-              onClick={() => buyFromSale(1)}
-              style={{ fontWeight: "bold", margin: "0 auto" }}
-            >
-              Grab a Whitelisted ICTuT for {_showListingPrice(whitelistPrice)} ICP
-            </Button></>
-              :
-              <p><strong><span style={{color:"red"}}>Sorry, you need to be whitelisted to buy right now, otherwise wait until 15:00 UTC! If you have been whitelisted and have not purchased an ICTuT, please wait as this can take a few seconds to load.</span></strong></p>
-            }
+              <p><strong><span style={{color:"red"}}>Please login to access this sale</span></strong></p>
+            </>
+            :
+            <>
+              {!loaded ? 
+                <>
+                  <p><strong><span style={{color:"red"}}>Please wait while we load the sale page...</span></strong></p>
+                </>
+                :
+                <>
+                {salesOnline ? 
+                  <>
+                    <p><strong>Please note:</strong> All transactions are secured via Entrepot's escrow platform. There are no refunds or returns, once a transaction is made it can not be reversed. Entrepot provides a transaction service only. By clicking the button below you show acceptance of our Terms of Service</p>
+                    <Grid container spacing={2} style={{}}>
+                      <Grid className={classes.stat} item sm={3}>
+                    <Button
+                      variant={"contained"}
+                      color={"primary"}
+                      onClick={() => buyFromSale(1)}
+                      style={{ fontWeight: "bold", margin: "0 auto" }}
+                    >
+                      Buy 1 ICTuT<br />for {_showListingPrice(150000000n)} ICP
+                    </Button>
+                    </Grid>
+                      <Grid className={classes.stat} item sm={3}>
+                    <Button
+                      variant={"contained"}
+                      color={"primary"}
+                      onClick={() => buyFromSale(5)}
+                      style={{ fontWeight: "bold", margin: "0 auto" }}
+                    >
+                      Buy 5 ICTuT<br />for {_showListingPrice(150000000n*5n)} ICP
+                    </Button>
+                    </Grid>
+                      <Grid className={classes.stat} item sm={3}>
+                    <Button
+                      variant={"contained"}
+                      color={"primary"}
+                      onClick={() => buyFromSale(10)}
+                      style={{ fontWeight: "bold", margin: "0 auto" }}
+                    >
+                      Buy 10 ICTuT<br />for {_showListingPrice(150000000n*10n)} ICP
+                    </Button>
+                    </Grid>
+                      <Grid className={classes.stat} item sm={3}>
+                    <Button
+                      variant={"contained"}
+                      color={"primary"}
+                      onClick={() => buyFromSale(20)}
+                      style={{ fontWeight: "bold", margin: "0 auto" }}
+                    >
+                      Buy 20 ICTuT<br />for {_showListingPrice(150000000n*20n)} ICP
+                    </Button>
+                    </Grid>
+                    </Grid>
+                    
+                    </>
+                  :
+                    <>
+                    <p><strong><span style={{color:"red"}}>Sorry, the sale is now over! You can grab your ICTuT from the marketplace soon!</span></strong></p>
+                    </>
+                  }
+                </>
+              }
             </>
           }
+          
+          
 
           </div>
         </div>
