564dae6a59d1f1e328b86d7e39842a6e2ab46a3c
Stephen Andrews <stephen@andrewstribe.co.nz>
2021-10-16 17:22:42 +1300

    fix images

--- src/components/Sold.js
+++ src/components/Sold.js
@@ -60,30 +60,6 @@ export default function Sold(props) {
 
     return "https://" +icbstorage[i % 10]+".raw.ic0.app/Token/"+i;
   };
-  const tutsimg = i => {
-    const icbstorage = ["bkf7n-yyaaa-aaaaj-qacfq-cai",
-    "dqzxr-giaaa-aaaaj-qackq-cai",
-    "dz24n-qaaaa-aaaaj-qacla-cai",
-    "d632z-5yaaa-aaaaj-qaclq-cai",
-    "ctv6x-sqaaa-aaaaj-qacma-cai",
-    "cuuyd-7iaaa-aaaaj-qacmq-cai",
-    "c5xt7-jaaaa-aaaaj-qacna-cai",
-    "c2wvl-eyaaa-aaaaj-qacnq-cai",
-    "cpreg-fqaaa-aaaaj-qacoa-cai",
-    "ciqcs-iiaaa-aaaaj-qacoq-cai",
-    "cbtjo-6aaaa-aaaaj-qacpa-cai",
-    "b7coa-zqaaa-aaaaj-qacga-cai",
-    "cgsp2-tyaaa-aaaaj-qacpq-cai",
-    "bydiu-uiaaa-aaaaj-qacgq-cai",
-    "bradi-caaaa-aaaaj-qacha-cai",
-    "bwbf4-pyaaa-aaaaj-qachq-cai",
-    "dm5na-riaaa-aaaaj-qaciq-cai",
-    "df6g4-haaaa-aaaaj-qacja-cai",
-    "dc7ai-kyaaa-aaaaj-qacjq-cai",
-    "dxyrf-lqaaa-aaaaj-qacka-cai"]
-
-    return "https://" +icbstorage[i % icbstorage.length]+".raw.ic0.app/?index="+i;
-  };
   const mintNumber = () => {
     if (props.collection === "bxdf4-baaaa-aaaah-qaruq-cai") return index;
     if (props.collection === "3db6u-aiaaa-aaaah-qbjbq-cai") return index;
@@ -94,7 +70,6 @@ export default function Sold(props) {
     if (props.collection === "3db6u-aiaaa-aaaah-qbjbq-cai") return "https://d3ttm-qaaaa-aaaai-qam4a-cai.raw.ic0.app?tokenId=" + index;
     if (props.collection === "q6hjz-kyaaa-aaaah-qcama-cai")
       return icpbunnyimg(index)
-    if (props.collection === "ahl3d-xqaaa-aaaaj-qacca-cai") return tutsimg(index);
     return "https://"+props.collection+".raw.ic0.app/?cc=0&type=thumbnail&tokenid=" + tokenid;
   };
   const nftLink = () => {
