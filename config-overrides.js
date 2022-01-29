962695ba47ae091ed31d2646deac5ddb917aca18
electrovir <electrovir@users.noreply.github.com>
2022-01-29 09:13:00 -0700

    update react-scripts and webpack
    Add polyfill for stream by including the stream-browserify dep and also
    adding react-app-rewired so that we can use config-overrides.js.
    Update scripts to use react-app-rewired instead of react-scripts.

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+module.exports = function override (config, env) {
+    let loaders = config.resolve;
+    loaders.fallback = {
+        "stream": false,
+    };
+    config.optimization = {
+        splitChunks: {
+            chunks: 'all',
+        },
+    };
+    
+    return config
+}
