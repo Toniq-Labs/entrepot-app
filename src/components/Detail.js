5e1e18e9f946f881ad3787bed7452b34171c1017
Bob Bodily <bob@Bobs-MacBook-Pro.local>
2022-06-10 09:15:52 -0600

    updated detail page for kvhs

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -308,6 +308,7 @@ const Detail = (props) => {
       case "gikg4-eaaaa-aaaam-qaieq-cai":
       case "bapzn-kiaaa-aaaam-qaiva-cai":
       case "4wiph-kyaaa-aaaam-qannq-cai":
+      case "3cjkh-tqaaa-aaaam-qan6a-cai":
         return extractEmbeddedImage(EntrepotNFTImage(canister, index, tokenid, true), classes);
       
       // default case is to just use the thumbnail on the detail page
