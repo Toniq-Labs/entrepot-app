fd0fe85d7a9df10f786052cf06ee946caeb99aea
electrovir <electrovir@users.noreply.github.com>
2022-06-18 03:24:31 +0000

    block Toniq Earn features for some countries

---
+++ src/location/geo-ip.js
@@ -0,0 +1,34 @@
+// these features are loaded in index.html via the geoip-js.com/js/apis/geoip2/v2.1/geoip2.js script
+
+const blockedToniqEarnCountryCodes = [
+    'us',
+    // these should get blocked by other means already, but lets throw them here just in case
+    'cf',
+    'cu',
+    'ir',
+    'kp',
+    'sy',
+];
+
+export async function checkIfToniqEarnAllowed() {
+    return new Promise((resolve) => {
+        if (window.location.hostname === 'localhost') {
+            // allow in localhost for testing
+            resolve(true);
+        }
+        try {
+            window.geoip2.country((result) => {            
+                const allowed = !blockedToniqEarnCountryCodes.includes(result.country.iso_code.toLowerCase())
+                resolve(allowed);
+            }, (error) => {
+                console.error(error);
+                // if any error occurs, resort to simply blocking the user from accessing Toniq Earn features
+                resolve(false);
+            });
+        } catch (error) {
+            console.error(error);
+            // if any error occurs, resort to simply blocking the user from accessing Toniq Earn features
+            resolve(false);
+        }
+    });
+}
