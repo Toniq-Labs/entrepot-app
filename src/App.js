bb73368cbf631ece9ed2ef8a4be45a1d861111c6
Stephen Andrews <stephen@andrewstribe.co.nz>
2022-06-28 19:08:06 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -364,7 +364,7 @@ export default function App() {
     return true;
   };
   const _processPaymentForCanister = async _collection => {
-    if (typeof _collection == 'undefined' || _collection || !_collection.hasOwnProperty('legacy') || !_collection.legacy) return true;
+    if (typeof _collection == 'undefined' || !_collection || !_collection.hasOwnProperty('legacy') || !_collection.legacy) return true;
     const _api = extjs.connect("https://boundary.ic0.app/", identity);
     var payments = await _api.canister(_collection.canister).payments();
     if (payments.length === 0) return true;
