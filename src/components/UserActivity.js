6f796d5b95a89bbb11e6a9d224fed316b33e419f
Robert Bodily <bodilyrobert@gmail.com>
2022-08-10 17:27:55 -0600

    Updating sorting on personal activity page

--- src/components/UserActivity.js
+++ src/components/UserActivity.js
@@ -352,8 +352,7 @@ export default function UserActivity(props) {
                     value={sort}
                     onChange={changeSort}
                   >
-                    <MenuItem value={"mint_number"}>Minting #</MenuItem>
-                    <MenuItem value={"nri"}>NFT Rarity Index</MenuItem>
+                    <MenuItem value={"most_recent"}>Most Recent</MenuItem>
                   </Select>
                 </FormControl>
               </Grid>
