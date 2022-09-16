8033c9c0f4f61976ac2f32202b9ba25c3fa6a70d
Robert Bodily <bodilyrobert@gmail.com>
2022-09-15 22:49:28 -0600

    Update CollectionDetails.js

--- src/components/CollectionDetails.js
+++ src/components/CollectionDetails.js
@@ -106,7 +106,7 @@ export default function CollectionDetails(props) {
         {size ? <h4 style={{marginTop:-20}}>Collection of {numberWithCommas(size)}</h4> : ""}
         {['commission'].filter(a => collection.hasOwnProperty(a) && collection[a]).map(a => {
           return (
-            <h4 style={{marginTop:-20}}>Creatory Royalty: {(100*(a-.01))}%</h4>
+            <h4 style={{marginTop:-20}}>Creatory royalty: {parseInt(100*(parseFloat(a)-.01))}%</h4>
           )})
         };
       {['kyc'].filter(a => collection.hasOwnProperty(a) && collection[a]).map(() => {
