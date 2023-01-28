1b81bb812f88dc739aee25d6d4fcbf99c0c2c2fe
ponnexcodev <emmanuelr@codev.com>
2023-01-28 11:20:53 +0800

    NFT Listing tab fixes

--- src/views/Listings/ListingsTabs.js
+++ src/views/Listings/ListingsTabs.js
@@ -16,7 +16,7 @@ export function ListingsTabs(props) {
     const classes = useStyles();
 
     return (
-        <>
+        <div className={classes.tabWrapper}>
             <StyledTabs
                 value={currentTab}
                 indicatorColor="primary"
@@ -29,46 +29,54 @@ export function ListingsTabs(props) {
             >
                 <StyledTab value="nfts" label="NFTs" />
                 <StyledTab value="activity" label="Activity" />
-                <div className={classes.viewControllerWrapper}>
-                    <ToniqIcon
-                        icon={LayoutGrid24Icon}
-                        onClick={() => {
-                            setGridSize('large');
-                            storeUserPreferences('gridSize', 'large');
-                            forceCheck();
-                        }}
-                        style={{
-                            color: gridSize === 'large' ? '#000000' : 'gray',
-                            cursor: 'pointer',
-                        }}
-                    />
-                    <ToniqIcon
-                        icon={GridDots24Icon}
-                        onClick={() => {
-                            setGridSize('small');
-                            storeUserPreferences('gridSize', 'small');
-                            forceCheck();
-                        }}
-                        style={{
-                            color: gridSize !== 'large' ? '#000000' : 'gray',
-                            cursor: 'pointer',
-                        }}
-                    />
-                </div>
             </StyledTabs>
-        </>
+            <div className={classes.viewControllerWrapper}>
+                <ToniqIcon
+                    icon={LayoutGrid24Icon}
+                    onClick={() => {
+                        setGridSize('large');
+                        storeUserPreferences('gridSize', 'large');
+                        forceCheck();
+                    }}
+                    style={{
+                        color: gridSize === 'large' ? '#000000' : 'gray',
+                        cursor: 'pointer',
+                    }}
+                />
+                <ToniqIcon
+                    icon={GridDots24Icon}
+                    onClick={() => {
+                        setGridSize('small');
+                        storeUserPreferences('gridSize', 'small');
+                        forceCheck();
+                    }}
+                    style={{
+                        color: gridSize !== 'large' ? '#000000' : 'gray',
+                        cursor: 'pointer',
+                    }}
+                />
+            </div>
+        </div>
     );
 }
 
 const useStyles = makeStyles(theme => ({
+    tabWrapper: {
+        position: 'relative',
+    },
     viewControllerWrapper: {
         alignItems: 'center',
         gap: '8px',
         display: 'none',
         justifyContent: 'end',
-        width: '100%',
+        width: 'auto',
         [theme.breakpoints.down('sm')]: {
             display: 'flex',
+            position: 'absolute',
+            right: 0,
+            top: 0,
+            bottom: 0,
+            margin: 'auto 0',
         },
     },
 }));
