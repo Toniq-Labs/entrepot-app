b7f1e8fe7a184387de1f469c906582e34c9d5eb3
ponnexcodev <emmanuelr@codev.com>
2023-04-11 07:00:13 +0800

    Frontend fixes

--- src/typescript/ui/elements/main-content-pages/sale-page/toniq-entrepot-sale-category-card.element.ts
+++ src/typescript/ui/elements/main-content-pages/sale-page/toniq-entrepot-sale-category-card.element.ts
@@ -208,7 +208,7 @@ export const EntrepotSaleCategoryCardElement = defineElement<{
         }
 
         .stat-entry ${ToniqChip} {
-            ${toniqFontStyles.boldLabelFont};
+            ${toniqFontStyles.boldParagraphFont};
             ${toniqFontStyles.monospaceFont};
             max-height: 24px;
             flex-grow: 1;
